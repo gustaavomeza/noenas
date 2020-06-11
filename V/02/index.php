4352ebe797cd5c7adf814a8e35e52505174dffe6
unknown <gustaavomeza@gmail.com>
2020-06-10 21:17:21 -0500

    up noenas web site

---
+++ V/02/index.php
@@ -0,0 +1,421 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>n o e n a s</title>
+    <link rel='stylesheet' href='bootstrap/css/bootstrap.min.css' type='text/css' />
+    <link rel='stylesheet' href='app.css?<?php echo rand(10, 99); ?>' type='text/css' />
+    <link rel="icon" href="favicon.ico" />
+
+    <style>
+        body,
+        html,
+        * {
+            font-family: "pxn" !important;
+        }
+        
+        .miidiomas {
+            padding: 0px;
+            display: inline;
+            margin: 0px;
+        }
+        
+        .miidiomas>li {
+            display: inline-block;
+        }
+        
+        .ido {
+            text-transform: uppercase;
+            margin: 0px;
+            padding: 0px;
+        }
+        
+        .ido>a {
+            background: #0d0d0d;
+            margin: 0px !important;
+            color: white;
+            padding: 2px 10px !important;
+            font-weight: 100;
+            border: solid 2px white;
+            border-radius: 20px;
+            font-size: 13px;
+        }
+        
+        .ido>.active {
+            background: white;
+            color: #0d0d0d;
+            font-weight: 600;
+        }
+        
+        .ba-mmap {
+            z-index: 200;
+        }
+        
+        .st3 {
+            cursor: pointer;
+            fill: #fff;
+        }
+    </style>
+
+    <script>
+        if (
+            /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(
+                navigator.userAgent
+            ) ||
+            /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+                navigator.userAgent.substr(0, 4)
+            )
+        ) {
+            var url = "mobil.html";
+            window.location = url;
+        }
+    </script>
+
+
+
+</head>
+
+
+
+<body class="home page-template page-template-page-templates 
+            page-template-front page-template-page-templatesfront-php 
+            page page-id-5 wp-custom-logo offcanvas">
+    <div class="off-canvas-wrapper">
+        <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+            <div class="off-canvas-content" data-off-canvas-content data-content-scroll="false">
+
+
+
+
+                <header class="ba-header">
+                    <div class="row column">
+                        <div class="ba-header-bar">
+                            <a href="#home" class="ba-extra-text ba-explore-dark" style="opacity: 0;width: 0px;" data-for-people data-anchor="home"></a>
+                            <a class="ba-logo" href="#home" rel="home" title="Noenas">
+                                <img src="noenaslogo.svg" alt="Noenas" style="height:25px" />
+                            </a>
+                            <ul class="miidiomas">
+                                <li class="nav-item ido mr-2">
+                                    <a class="nav-link smoth-scroll active" href="#">ENGLISH</a>
+                                </li>
+                                <li class="nav-item ido">
+                                    <a class="nav-link smoth-scroll" href="#">FRANÇAIS</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </header>
+
+
+
+
+
+
+                <div class="ba-loader-shades" data-loader-shades></div>
+                <div class="ba-loader" data-loader></div>
+
+                <main id="fullpage">
+
+                    <section class="section fp-noscroll" data-anchor="home" data-section="home">
+                        <div class="ba-section ba-section--intro">
+                            <div class="ba-section-body">
+                                <div class="row">
+                                    <div class="column small-12 hide-for-large">
+                                        <div class="ba-ghost-shape"></div>
+                                    </div>
+                                    <div class="column medium-8 large-4 small-12 position-relative" data-box>
+                                        <div data-box-content>
+                                            <h2 class="ba-section-title" data-title>
+                                                INVESTING INTO<br> NON-ECONOMIC<br> CONSEQUENCES OF<br> ECONOMIC ACTIONS
+                                            </h2>
+                                            <div data-text>
+                                                <p>We are a corporation with a number of subsidiary<br>companies under its control, which<br>are diversified in different industries. We carry out all the equity financing activities to meet the equity financing
+                                                    needs of our companies</p>
+
+                                            </div>
+                                            <a href="#projects" class="ba-extra-text ba-scroll-link">
+                                                <span data-scroll>
+                                                    SCROLL
+                                                </span>
+                                            </a>
+                                        </div>
+                                        <div class="ba-section-line" data-sline></div>
+                                        <!-- /.ba-section -->
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </section>
+
+
+
+
+
+
+
+
+                    <!--  LISTA MENU   -->
+                    <section class="section fp-section-projects" data-anchor="projects" data-section="projects">
+                        <div class="ba-section ba-section-projects">
+                            <div class="ba-section-body">
+                                <div class="row">
+                                    <div class="column medium-12 large-6 small-12 position-relative" data-box>
+                                        <div data-box-content>
+
+
+
+
+                                            <div class="ba-projects-slider show-for-large" data-projects-slider>
+
+
+
+
+                                                <div>
+                                                    <h2 class="h1 ba-section-title" data-title>1</h2>
+                                                    <div class="ba-section-text" data-text>
+                                                        <p>Nozxc</p>
+                                                    </div>
+                                                    <a href="#contact" class="ba-extra-text ba-scroll-link" data-scroll>
+                                                        <span data-scroll>
+                                                            SCROLL
+                                                        </span>
+                                                    </a>
+                                                </div>
+
+
+
+
+
+                                                <div>
+                                                    <h2 class="h1 ba-section-title" data-title>2</h2>
+                                                    <div class="ba-section-text" data-text>
+                                                        <p>Nozxc</p>
+                                                    </div>
+                                                    <a href="#contact" class="ba-extra-text ba-scroll-link" data-scroll>
+                                                        <span data-scroll>
+                                                            SCROLL
+                                                        </span>
+                                                    </a>
+                                                </div>
+
+
+
+                                                <div>
+                                                    <h2 class="h1 ba-section-title" data-title>3</h2>
+                                                    <div class="ba-section-text" data-text>
+                                                        <p>Nozxc</p>
+                                                    </div>
+                                                    <a href="#contact" class="ba-extra-text ba-scroll-link" data-scroll>
+                                                        <span data-scroll>
+                                                            SCROLL
+                                                        </span>
+                                                    </a>
+                                                </div>
+
+                                                <div>
+                                                    <h2 class="h1 ba-section-title" data-title>4</h2>
+                                                    <div class="ba-section-text" data-text>
+                                                        <p>Nozxc</p>
+                                                    </div>
+                                                    <a href="#contact" class="ba-extra-text ba-scroll-link" data-scroll>
+                                                        <span data-scroll>
+                                                            SCROLL
+                                                        </span>
+                                                    </a>
+                                                </div>
+
+
+
+
+                                            </div>
+
+
+
+                                        </div>
+                                        <div class="ba-section-line" data-sline></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <!--  LISTA MENU 5d5d5d   -->
+
+
+
+
+
+
+
+
+
+
+
+                    <section class="section fp-section-contact fp-noscroll" data-anchor="contact" data-section="contact" style="height: 305px;">
+
+
+
+
+                        <div class="ba-section ba-section-contact" id="contact-inner">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                            <div class="offcanvas-menu" data-map="map.svg">
+
+
+
+
+
+
+                                <div class="ba-section-body">
+                                    <div class="row column">
+
+                                        <div class="ba-offcanvas-contacts text-right">
+                                            <p>Registry Code: 12504237<br>
+                                                <a href="tel:+3725096235">+372 50 962 35</a><br>
+                                                <a href="mailto:info@burfa.com">info@burfa.com</a></p>
+                                            <p>10117, Harju maakond, Tallinn<br> Kesklinna linnaosa, Tartu mnt 83</p>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+
+
+
+
+                                <svg class="ba-mmap" width="100%" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 1280 800" xml:space="preserve" fill="#515250">
+                                        <circle class="st0" cx="724" cy="515" r="7">
+                                            <animate  accumulate="none" additive="replace" attributeName="r" begin="0s" calcMode="linear" dur="1.5s" fill="remove" from="5" repeatCount="indefinite" restart="always" to="18">
+                                            </animate>
+                                            <animate  accumulate="none" additive="replace" attributeName="opacity" begin="0s" calcMode="linear" dur="1.5s" fill="remove" from="1" repeatCount="indefinite" restart="always" to="0">
+                                            </animate>
+                                            </circle>
+                                        <circle class="st3" cx="724" cy="515" r="7"/>
+                                </svg>
+
+                            </div>
+                            <div class="ba-contact-slider" data-contact-slider data-scroll-reveal></div>
+                            <div class="row" data-scroll-reveal data-scroll-trigger="0.8"></div>
+                        </div>
+                    </section>
+
+
+                    <div class="ba-loader-bg" data-loader-bg></div>
+
+
+                    <div class="ba-projects-bar ba-projects-bar--desk show-for-large">
+                        <div class="ba-projects-bar-wrap" data-projects-scroll-wrap>
+                            <div class="ba-projects-bar-wrap__fade ba-projects-bar-wrap__fade--top" data-projects-scroll-fade></div>
+                            <div data-projects-scroll-list>
+                                <ul class="ba-projects-list" data-projects-list>
+                                    <li class="ba-project" data-projects-list-item>
+                                        <img class="ba-project__img" src="noenasmenu.svg">
+                                        <span class="ba-extra-text" data-project-list-title>
+                                            TEXTILES
+                                        </span>
+                                        <span class="ba-project__line" data-line></span>
+                                        <span class="ba-project__line-h" data-line-h></span>
+                                    </li>
+                                    <li class="ba-project" data-projects-list-item>
+                                        <img class="ba-project__img" src="noenasmenu.svg">
+                                        <span class="ba-extra-text" data-project-list-title>
+                                            TECH
+                                        </span>
+                                        <span class="ba-project__line" data-line></span>
+                                        <span class="ba-project__line-h" data-line-h></span>
+                                    </li>
+                                    <li class="ba-project" data-projects-list-item>
+                                        <img class="ba-project__img" src="noenasmenu.svg">
+                                        <span class="ba-extra-text" data-project-list-title>
+                                            FOOD
+                                        </span>
+                                        <span class="ba-project__line" data-line></span>
+                                        <span class="ba-project__line-h" data-line-h></span>
+                                    </li>
+                                    <li class="ba-project" data-projects-list-item>
+                                        <img class="ba-project__img" src="noenasmenu.svg">
+                                        <span class="ba-extra-text" data-project-list-title>
+                                            REAL ESTATE
+                                        </span>
+                                        <span class="ba-project__line" data-line></span>
+                                        <span class="ba-project__line-h" data-line-h></span>
+                                    </li>
+
+
+                                </ul>
+
+                            </div>
+                            <div class="ba-projects-bar-wrap__fade ba-projects-bar-wrap__fade--bot" data-projects-scroll-fade></div>
+                        </div>
+                    </div>
+                </main>
+
+
+
+
+
+                <div class="ba-section ba-section--shape">
+                    <div class="ba-section-body">
+                        <div class="row">
+                            <div class="column large-6 large-offset-6">
+                                <div class="ba-shape" id="container"></div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
+
+
+
+
+
+            </div>
+        </div>
+    </div>
+    <script defer src="three.min.js"></script>
+    <script src="TweenMax.min.js"></script>
+    <script src="ScrollToPlugin.min.js"></script>
+    <script type='text/javascript' src='jquery.min.js'></script>
+    <script type='text/javascript' src='ScrollMagic.min.js'></script>
+    <script type='text/javascript' src='animation.gsap.js'></script>
+    <script type='text/javascript' src='pwaforwp.min.js?ver=1.7.7'></script>
+    <script type='text/javascript' src='thickbox.js'></script>
+    <script type='text/javascript' src='popper.min.js'></script>
+    <script type='text/javascript' src='bootstrap/js/bootstrap.min.js'></script>
+    <script>
+        $(".st3").click(function() {
+            var url = "";
+            window.open(url, "_blank");
+        });
+
+
+        var three = document.createElement("script");
+        var app = document.createElement("script");
+        TweenMax.to('[data-loader-logo]', 0, {
+            opacity: 1,
+            onComplete: function() {
+                three.src = "three.min.js";
+                app.src = "app.js?<?php echo rand(10, 99); ?>";
+                document.body.appendChild(app);
+            }
+        })
+    </script>
+</body>
+
+</html>
---
+++ mobil.php
@@ -0,0 +1,205 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <title>n o e n a s</title>
+    <link rel='stylesheet' href='bootstrap/css/bootstrap.min.css' type='text/css' />
+    <link rel='stylesheet' href='appmobil.css?<?php echo rand(10, 99); ?>' type='text/css' />
+    <link rel="icon" href="favicon.ico" />
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <style>
+        body,
+        html,
+        * {
+            font-family: "pxn" !important;
+        }
+        
+        body {
+            background: #1a1a1a !important;
+        }
+        
+        .miidiomas {
+            text-align: right;
+            padding: 0px;
+            display: inline;
+            margin: 0px;
+        }
+        
+        .miidiomas>li {
+            display: inline-block;
+        }
+        
+        .ido {
+            text-transform: uppercase;
+            margin: 0px;
+            padding: 0px;
+        }
+        
+        .ido>a {
+            background: #0d0d0d;
+            margin: 0px !important;
+            color: white;
+            padding: 2px 10px !important;
+            font-weight: 100;
+            border: solid 2px white;
+            border-radius: 20px;
+            font-size: 8px;
+        }
+        
+        .ido>.active {
+            background: white;
+            color: #0d0d0d;
+            font-weight: 600;
+        }
+        
+        .ba-mmap {
+            z-index: 200;
+        }
+        
+        .st3 {
+            cursor: pointer;
+            fill: #fff;
+        }
+    </style>
+</head>
+
+
+
+<body class="home page-template page-template-page-templates 
+            page-template-front page-template-page-templatesfront-php 
+            page page-id-5 wp-custom-logo offcanvas">
+    <div class="off-canvas-wrapper">
+        <div class="off-canvas-wrapper-inner">
+            <div class="off-canvas-content">
+
+
+
+
+                <header class="ba-header" style="width: 100%;">
+                    <div class="row column">
+                        <div class="ba-header-bar">
+                            <a href="#home" class="ba-extra-text ba-explore-dark" style="opacity: 0;width: 0px;" data-for-people data-anchor="home"></a>
+                            <a class="ba-logo" href="#home" rel="home" title="Noenas">
+                                <img src="noenaslogo.svg" alt="Noenas" style="height:22px" />
+                            </a>
+                            <ul class="miidiomas">
+                                <li class="nav-item ido mr-2">
+                                    <a class="nav-link smoth-scroll active" href="#">ENGLISH</a>
+                                </li>
+                                <li class="nav-item ido">
+                                    <a class="nav-link smoth-scroll" href="#">FRANÇAIS</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </header>
+
+                <center><br><br><br><br>
+                    <img src="aniani.gif" />
+                </center><br><br>
+
+                <center>
+                    <h2 class="ba-section-title" data-title style="font-size: 22px; margin:0px 30px;">
+                        INVESTING INTO<br> NON-ECONOMIC<br> CONSEQUENCES OF<br> ECONOMIC ACTIONS
+                    </h2><br><br>
+                    <p style="font-size: 15px; margin:0px 30px;">We are a corporation with a number of subsidiary<br>companies under its control, which<br>are diversified in different industries. We carry out all the equity financing activities to meet the equity financing needs of our companies</p><br>
+
+                    <p style="font-size: 15px; margin:0px 30px;">We only focus on Ethical Finance principles such as Transparency, Sobriety, Accountability and Efficiency to help create a society that protect and promotes quality of life and human dignity for all</p>
+                </center><br><br><br><br>
+
+
+
+
+                <ul class="ba-projects-list">
+                    <li class="ba-project" data-aos="fade-right" data-aos-offset="200" data-aos-delay="50" data-aos-easing="ease-in-sine">
+                        <img class="ba-project__img" src="noenasmenu.svg">
+                        <span class="ba-extra-t ext">
+                                            TEXTILES
+                                        </span>
+                        <span class="ba-project__line" data-line></span>
+                        <span class="ba-project__line-h" data-line-h></span>
+                    </li>
+                    <li class="ba-project" data-aos="fade-right" data-aos-offset="200" data-aos-delay="100" data-aos-easing="ease-in-sine">
+                        <img class="ba-project__img" src="noenasmenu.svg">
+                        <span class="ba-extra-text">
+                                            TECH
+                                        </span>
+                        <span class="ba-project__line" data-line></span>
+                        <span class="ba-project__line-h" data-line-h></span>
+                    </li>
+                    <li class="ba-project" data-aos="fade-right" data-aos-offset="200" data-aos-delay="150" data-aos-easing="ease-in-sine">
+                        <img class="ba-project__img" src="noenasmenu.svg">
+                        <span class="ba-extra-text">
+                                            FOOD
+                                        </span>
+                        <span class="ba-project__line" data-line></span>
+                        <span class="ba-project__line-h" data-line-h></span>
+                    </li>
+                    <li class="ba-project" data-aos="fade-right" data-aos-offset="200" data-aos-delay="200" data-aos-easing="ease-in-sine">
+                        <img class="ba-project__img" src="noenasmenu.svg">
+                        <span class="ba-extra-text">
+                                            REAL ESTATE
+                                        </span>
+                        <span class="ba-project__line" data-line></span>
+                        <span class="ba-project__line-h" data-line-h></span>
+                    </li>
+
+
+                </ul><br><br><br><br><br><br>
+
+
+
+                <div class="ba-section-body">
+                    <div class="row column">
+
+                        <div class="ba-offcanvas-contacts text-right" style="float: right;">
+                            <p class="mb-3" style="font-weight: 700;font-size: 16px;">Headquarter</p>
+                            <p style="margin-bottom: -6px;">20 des Souers Grises, Montreal,</p>
+                            <p class="mb-3">Quebec, Canada, H3C 5M1</p>
+                            <p class="mb-3">
+                                <a href="tel:+1 514 962 0493">+1 514 962 0493</a>
+                            </p>
+                            <p>
+                                <a href="mailto:info@noenas.com">info@noenas.com</a>
+                            </p><br>
+
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div onClick="irMapa(1);">
+                    <?php require_once('mapa2.php'); ?>
+                </div>
+
+
+
+
+
+
+            </div>
+        </div>
+    </div>
+    <script type='text/javascript' src='jquery.min.js'></script>
+    <script type='text/javascript' src='popper.min.js'></script>
+    <script type='text/javascript' src='bootstrap/js/bootstrap.min.js'></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+    <script>
+
+        AOS.init();
+
+        function irMapa() {
+            var url = "https://www.google.com/maps/place/20+Rue+des+Soeurs+Grises,+Montr%C3%A9al,+QC+H3C+5M1,+Canad%C3%A1/@45.4984018,-73.5529588,17z/data=!4m5!3m4!1s0x4cc91af5e668fe83:0x80034759d1b5f82d!8m2!3d45.4981837!4d-73.5535274";
+            window.open(url, "_blank");
+        }
+     
+    </script>
+
+</body>
+
+</html>
